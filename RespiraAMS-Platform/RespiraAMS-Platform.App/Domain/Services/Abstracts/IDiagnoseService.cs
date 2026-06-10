b0d071219f30aa1ec507f6bc4762342c557f4eeb
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2026-06-10 21:34:33 +0700

    feat(app service): add diagnose service

---
+++ RespiraAMS-Platform/RespiraAMS-Platform.App/Domain/Services/Abstracts/IDiagnoseService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+using Domain.Models;
+
+namespace Domain.Services.Abstracts;
+
+public interface IDiagnoseService
+{
+    (Severity, TreatmentSite) Curb65(bool confusion, double? urea, int respiratory, int systolic, int diastolic, int age);
+    bool NeedIcu(List<IcuHospitalizeCriterion> criteria, int mainThreshold, int secondaryThreshold, List<Guid> options);
+    Dictionary<Pathogen, double> AssessInfectionProbability(List<ResistanceRiskFactor> factors, List<Guid> options);
+}
