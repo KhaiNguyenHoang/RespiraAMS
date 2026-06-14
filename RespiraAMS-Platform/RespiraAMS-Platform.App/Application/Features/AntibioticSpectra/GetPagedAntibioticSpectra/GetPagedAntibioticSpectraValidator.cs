c220462c1cd2e3a5cddad8b56871855d0cc31dec
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2026-06-14 15:04:52 +0700

    fix(app service): add validation for get paged antibiotic spectra features

---
+++ RespiraAMS-Platform/RespiraAMS-Platform.App/Application/Features/AntibioticSpectra/GetPagedAntibioticSpectra/GetPagedAntibioticSpectraValidator.cs
@@ -0,0 +1,12 @@
+﻿using Application.Shared.Validations;
+using FluentValidation;
+
+namespace Application.Features.AntibioticSpectra.GetPagedAntibioticSpectra;
+
+public class GetPagedAntibioticSpectrumValidator : AbstractValidator<GetPagedAntibioticSpectraQuery>
+{
+    public GetPagedAntibioticSpectrumValidator()
+    {
+        RuleFor(query => query.Param).IsValidPaginationParam();
+    }
+}
---
+++ RespiraAMS-Platform/RespiraAMS-Platform.App/Application/Shared/Validations/CustomValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using Application.Shared.Dtos;
+using FluentValidation;
+
+namespace Application.Shared.Validations;
+
+public static class CustomValidationRule
+{
+    public static IRuleBuilderOptions<T, PaginationParam> IsValidPaginationParam<T>(
+        this IRuleBuilder<T, PaginationParam> ruleBuilder)
+    {
+        return ruleBuilder
+            .Must(p => p.Page > 0)
+            .WithMessage("Pagination page must be a positive integer")
+            .Must(p => p.Size > 0)
+            .WithMessage("Pagination size must be a positive integer");
+    }
+}
