c90e58dd6d7eec0d12a5c4199b48aa1b72e8d3fd
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2026-06-14 14:30:00 +0700

    feat(shared block): add pagination DTOs in Share block

---
+++ RespiraAMS-Platform/RespiraAMS-Platform.Shared/DTOs/Pagination.cs
@@ -0,0 +1,23 @@
+﻿namespace RespiraAMS_Platform.Shared.DTOs;
+
+public class PaginationParam
+{
+    public int Page { get; set; } = 1;
+    public int Size { get; set; } = 10;
+}
+
+public class PaginationMetadata
+{
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public int TotalItemCount { get; set; }
+    public int PageCount { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+}
+
+public class Pagination<T>(PaginationMetadata metadata, IEnumerable<T> items)
+{
+    public PaginationMetadata Metadata { get; set; } = metadata;
+    public IEnumerable<T> Items { get; set; } = items;
+}
